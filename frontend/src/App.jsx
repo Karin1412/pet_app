877ce43d14c0a56026d30f2b0485bc4727914799
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-05-11 13:12:53 +0700

    home ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
+import Sidebar from "./components/common/SideBar"
 
 function App() {
   
 
   return (
     <div className="flex ">
-     <Routes>
+      <Sidebar></Sidebar>
+      <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
-    </Routes>
+      </Routes>
 
     </div>
   )
