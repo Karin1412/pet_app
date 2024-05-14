c4e83772ce81368741d18e20d0f6156a16e57616
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-05-14 19:11:30 +0700

    profile fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,26 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 import Sidebar from "./components/common/SideBar"
 import RightPanel from "./components/common/RightPanel"
+
 
 function App() {
   
 
   return (
     <div className="flex ">
-      
+      <Sidebar></Sidebar>
       <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+      <Route path="/notifications" element={<NotificationPage></NotificationPage>}></Route>
+      <Route path="/profile/:username" element={<ProfilePage></ProfilePage>}></Route>
       </Routes>
-      
+      <RightPanel></RightPanel>
     </div>
   )
 }
