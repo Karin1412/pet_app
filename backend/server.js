61c18978b47172d10af1621e05689724a0f6c2da
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-05-15 16:11:29 +0700

    profile api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+
 import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser"
 import { v2 as cloudinary } from "cloudinary";
@@ -18,6 +21,7 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({extended: true}))
@@ -28,6 +32,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
